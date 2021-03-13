6696884a5b8ff1954bbebb987149c6bd33ec7dd0
vikashmadhow <vikash.madhow@gmail.com>
2021-03-13 14:28:07 +0400

    Made into Java 9 module (added module-info)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+/**
+ * @author Vikash Madhow (vikash.madhow@gmail.com)
+ */
+module ma.vi.base {
+  requires java.sql;
+  requires jdk.unsupported;
+
+  requires org.codehaus.stax2;
+  requires org.yaml.snakeyaml;
+
+  exports ma.vi.base.cache;
+  exports ma.vi.base.circular;
+  exports ma.vi.base.collections;
+  exports ma.vi.base.config;
+  exports ma.vi.base.crypt;
+  exports ma.vi.base.io;
+  exports ma.vi.base.lang;
+  exports ma.vi.base.reflect;
+  exports ma.vi.base.string;
+  exports ma.vi.base.trie;
+  exports ma.vi.base.tuple;
+  exports ma.vi.base.unionfind;
+  exports ma.vi.base.util;
+  exports ma.vi.base.xml;
+}
